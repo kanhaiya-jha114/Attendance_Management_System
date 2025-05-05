ac43baf5ae342ba1bfa38b4d17f5e913072f67a5
kanhaiya-jha114 <kains948@gmail.com>
2025-05-05 09:05:57 +0530

    pushed

---
+++ Admin/Includes/sidebar.php
@@ -0,0 +1,57 @@
+ <ul class="navbar-nav sidebar sidebar-light accordion " id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center bg-gradient-primary  justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon" >
+          
+        </div>
+        <div class="sidebar-brand-text mx-3">AMS</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Class
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="createClass.php">
+          <i class="fas fa-chalkboard"></i>
+          <span><big>Create Class</big></span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Class Arms
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="createClassArms.php">
+          <i class="fas fa-code-branch"></i>
+          <span><big>Create Class Arms</big></span>
+        </a>
+      </li>
+       <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Teachers
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="createClassTeacher.php">
+          <i class="fas fa-chalkboard-teacher"></i>
+          <span><big>Create Class Teachers</big></span>
+        </a>
+      </li>
+
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Students
+      </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="createStudents.php">
+          <i class="fas fa-user-graduate"></i>
+          <span><big>Create Students</big></span>
+        </a>
+      </li>
+     
+    </ul>
---
+++ ClassTeacher/Includes/sidebar.php
@@ -0,0 +1,62 @@
+ <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center bg-gradient-primary justify-content-center" href="index.php">
+        <div class="sidebar-brand-text mx-3">AMS</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li> 
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Students
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="viewStudents.php">
+          <i class="fas fa-user-graduate"></i>
+          <span><big>View Students</big></span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Take Attendance
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="takeAttendance.php">
+          <i class="fas fa-user-graduate"></i>
+          <span><big>Take Attendance</big></span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+      View Class Attendance
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="viewAttendance.php">
+          <i class="fas fa-user-graduate"></i>
+          <span><big>View Class Attendance</big></span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+      View Student Attendance
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="viewStudentAttendance.php">
+          <i class="fas fa-user-graduate"></i>
+          <span><big>View Student Attendance</big></span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+      Today's Report (xls)
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="downloadRecord.php">
+          <i class="fas fa-user-graduate"></i>
+          <span><big>Download</big></span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+    </ul>
